1479b3751cb039d0e7377853b1cb951733e4da49
chenyuefeng <chenyuefeng>
2023-03-28 20:17:27 +0800

    更新Dockerfile

--- src/SuperLink.Api/ShortLinkApiConsts.cs
+++ src/SuperLink.Api/ShortLinkApiConsts.cs
@@ -1,24 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-
-namespace SuperShortLink.Api
+﻿namespace SuperShortLink.Api
 {
     /// <summary>
     /// 常量
     /// </summary>
     internal class ShortLinkApiConsts
     {
-        public static string WaitCheckLog => $"短链-待检查";
-
-        public static string Log => $"短链日志";
-
-        /// <summary>
-        /// 配置key
-        /// </summary>
-        public const string ConfigurationKey = "Export";
-
         /// <summary>
         /// 请求URL
         /// </summary>
@@ -26,6 +12,5 @@ namespace SuperShortLink.Api
         {
             public static string Generate = "/api/shortlink/generate";
         }
-
     }
 }
